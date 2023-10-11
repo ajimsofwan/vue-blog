c1606b4fa03112aa3847e7fe2dd77c075be3f199
Aji Muhammad Sofwan <ajimsofwan@gmail.com>
2023-10-11 18:53:05 +0700

    feat: add and configure tailwind-css + next-router

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0d9488",
+        "primary-light": "#14b8a6",
+        "primary-extra-light": "#5eead4",
+        "primary-dark": "#0f766e",
+      },
+    },
+  },
+  plugins: [],
+};
